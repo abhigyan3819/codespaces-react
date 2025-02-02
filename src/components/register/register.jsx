141bb1cf08bf7b3c0acd01ff3dd697885b6b0d38
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-02 22:59:06 +0530

    Update register.jsx

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react'
-import "./register.css"
+import React, { useState } from 'react';
+import "./register.css";
 
 const Register = () => {
-  const [IsLogin, setIsLogin] = useState(false)
+  const [IsLogin, setIsLogin] = useState(false);
+
   return (
     <div className='container'>
         <div className="login">
           <div className="switcher">
-            <button>Login</button>
-            <button>Sign In</button>
+            <button 
+              className={IsLogin ? "active" : ""}
+              onClick={() => setIsLogin(true)}
+            >Login</button>
+            <button 
+              className={!IsLogin ? "active" : ""}
+              onClick={() => setIsLogin(false)}
+            >Sign In</button>
           </div>
-            <div className="title">{IsLogin ? "Login" : "Sign in"}</div>
-            {IsLogin ?
+
+          <div className="title">{IsLogin ? "Login" : "Sign in"}</div>
+
+          {IsLogin ? (
             <div>
-            <form>
+              <form>
                 <input type="text" name="email" placeholder='Enter your email'/>
                 <input type="password" name='password' placeholder='Enter your password'/>
                 <button type='submit'>Login</button>
-            </form>
+              </form>
             </div>
-            :
+          ) : (
             <div>
-            <form>
-              <input type="text" name='username' placeholder='Enter your username' />
-              <input type="email" name='email' placeholder='Enter your email' />
-              <input type="password" name='password' placeholder='Enter your password' />
-
-              <button type="submit">Sign In</button>
-            </form>
+              <form>
+                <input type="text" name='username' placeholder='Enter your username' />
+                <input type="email" name='email' placeholder='Enter your email' />
+                <input type="password" name='password' placeholder='Enter your password' />
+                <button type="submit">Sign In</button>
+              </form>
             </div>
-            }
+          )}
         </div>
     </div>
-  )
+  );
 }
 
-export default Register 
+export default Register;
