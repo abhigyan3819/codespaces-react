4ee1552386ddce42933001171e6ff7e83ddc2c52
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-03 22:34:37 +0530

    Update globalStates.js

--- src/backend/globalStates.js
+++ src/backend/globalStates.js
@@ -3,12 +3,15 @@ import React, { createContext, useContext, useState } from 'react';
 const GlobalStateContext = createContext();
 export const GlobalStateProvider = ({ children }) => {
   const [currentChatUID, setcurrentChatUID] = useState(null);
+  const [messages, setMessages] = useState(null);
   const changeCurrentChatUID = (UID) => {
     setcurrentChatUID(UID);
   };
-
+  const updateMessages = (messages) => {
+    setMessages(messages);
+  };
   return (
-    <GlobalStateContext.Provider value={{ currentChatUID, setcurrentChatUID }}>
+    <GlobalStateContext.Provider value={{ currentChatUID, changeCurrentChatUID, messages, updateMessages }}>
       {children}
     </GlobalStateContext.Provider>
   );
