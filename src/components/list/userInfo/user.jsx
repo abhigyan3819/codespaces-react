b12675ac2d926cc09d0d014271519d6ff9751f24
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-03 11:31:24 +0530

    Update user.jsx

--- src/components/list/userInfo/user.jsx
+++ src/components/list/userInfo/user.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./user.css"
-import { fetchUserData }from '../../../backend/userInfo'
+import { fetchUserData } from '../../../backend/userInfo'  
 import { auth } from '../../../backend/firebase'
 
 const User = () => {
-  const uid = auth.currentUser.uid
-  const userData = fetchUserData(uid)
+  const [userData, setUserData] = useState(null);  
+  const uid = auth.currentUser?.uid; 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (uid) {
+        const data = await fetchUserData(uid);  
+        setUserData(data);  
+      }
+    };
+
+    fetchData(); 
+  }, [uid]);  
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className='userInfo'>
         <div className='user'>
             <img src="./profile.png" alt="pic" />
-            <div>{userData.username}</div>
+            <div>{userData.username}</div> 
         </div>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
