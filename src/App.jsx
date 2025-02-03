c6612ceb7039316156adee9192a22f1c1e185645
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-03 02:33:19 +0000

    updqte

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import './App.css';
 import Chat from './components/chat/chat';
 import List from './components/list/List';
 import Register from './components/register/register';
+import { Notification } from './components/Notification';
 function App() {
-  
+  const user = false
   return (
     <div className="container">
-      <Register/>
+      {user ?( 
+      <>
+       <List/>
+       <Chat/>
+      </>):(
+      <Register/>)}
+      <Notification />
     </div>
   );
 }
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,9 +1,26 @@
 import React, { useState } from 'react';
 import "./register.css";
+import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../backend/firebase';
 
 const Register = () => {
   const [IsLogin, setIsLogin] = useState(false);
-
+  const handleLogin =(e)=>{
+    e.preventDefault();
+    toast.success("Login")
+  }
+  const handleSignin = async (e)=>{
+    e.preventDefault()
+    const formData = new Form(e.target)
+    const { username, email, password } = Object.fromEntries(formData)
+    try{
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+      toast.success("Account successfully created !")
+    }catch(err){
+      toast.error(err.message)
+    }
+  }
   return (
     <div className='container'>
         <div className="login">
@@ -22,7 +39,7 @@ const Register = () => {
 
           {IsLogin ? (
             <div>
-              <form>
+              <form onSubmit={handleLogin}>
                 <input type="text" name="email" placeholder='Enter your email'/>
                 <input type="password" name='password' placeholder='Enter your password'/>
                 <button type='submit'>Login</button>
@@ -30,7 +47,7 @@ const Register = () => {
             </div>
           ) : (
             <div>
-              <form>
+              <form onSubmit={handleSignin}>
                 <input type="text" name='username' placeholder='Enter your username' />
                 <input type="email" name='email' placeholder='Enter your email' />
                 <input type="password" name='password' placeholder='Enter your password' />
