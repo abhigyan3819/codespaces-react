a4df3836ff03b60308c4aa90ba68ac6d591e2f63
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-03 15:09:42 +0530

    Update chatList.jsx

--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,87 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import "./chatList.css"
-import { auth } from '../../../backend/firebase'
-import { fetchUserData } from '../../../backend/userInfo'
+import React, { useEffect, useState } from 'react';
+import "./chatList.css";
+import { auth } from '../../../backend/firebase';
+import { fetchUserData } from '../../../backend/userInfo';
+import { doc, getDoc } from "firebase/firestore"; 
+import { db } from '../../../backend/firebase';
+
 const ChatList = () => {
-  const uid = auth.currentUser.uid
-  const [user, setUser] = useState(null)
-  const [friendsUID, setFriendsUID] = useState([])
-  const [friends, setFriends] = useState([])
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      if(uid){
-        const data = await fetchUserData(uid)
-        setUser(data)
-        setFriendsUID(user?.friends)
-        for(const friendUID in friendsUID){
-          try {
-            const userDocRef = doc(db, "users", friendUID);    
-            const docSnapshot = await getDoc(userDocRef);    
-            if (docSnapshot.exists()) {
-              const userData = docSnapshot.data();
-              setFriendsUID(userData)
-            } else {
-              console.log("unknown user");
-            }
-          } catch (err) {
-            console.log(err)
-          }
+  const [user, setUser] = useState(null);
+  const [friendsUID, setFriendsUID] = useState([]);
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const uid = currentUser.uid;
+        const data = await fetchUserData(uid);
+
+        if (data) {
+          setUser(data);
+          setFriendsUID(data.friends); 
+
+          const friendsData = await Promise.all(
+            data.friends.map(async (friendUID) => {
+              try {
+                const userDocRef = doc(db, "users", friendUID);
+                const docSnapshot = await getDoc(userDocRef);
+                if (docSnapshot.exists()) {
+                  return docSnapshot.data(); 
+                } else {
+                  console.log("Unknown user:", friendUID);
+                  return null;
+                }
+              } catch (err) {
+                console.log("Error fetching friend:", err);
+                return null;
+              }
+            })
+          );
+
+          setFriends(friendsData.filter(friend => friend !== null));
         }
-    }
-  } 
-  fetchData()
-  },[uid])
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='chatList'>
-        <div className='search'>
-            <div className='searchBar'>
-                <input type="search" placeholder='Search'/>
-            </div>
+      <div className='search'>
+        <div className='searchBar'>
+          <input type="search" placeholder='Search'/>
         </div>
-        <div className='item'>
-            <img src="./profile.png" alt="" />
-            <div className='texts'> 
-              <div className='name'>name</div>
-              <div className='lastmsg'>msg</div>
-            </div>
-        </div>
-        <div className='item'>
-          <img src="./profile.png" alt="" />
+      </div>
+
+      {friends.map((friend, index) => (
+        <div key={index} className='item'>
+          <img src="./profile.png" alt="Profile" />
           <div className='texts'>
-            <div className='name'>name</div>
-            <div className='lastmsg'>msg</div>
+            <div className='name'>{friend.username}</div>
+            <div className='lastmsg'>Last message...</div> 
           </div>
         </div>
-        <div className='item'>
-          <img src="./profile.png" alt="" />
-          <div className='texts'>
-            <div className='name'>name</div>
-            <div className='lastmsg'>msg</div>
-          </div>
-        </div>
-        <div className='item'>
-          <img src="./profile.png" alt="" />
-          <div className='texts'>
-            <div className='name'>name</div>
-            <div className='lastmsg'>msg</div>
-          </div>
-        </div>
-        <div className='item'>
-          <img src="./profile.png" alt="" />
-          <div className='texts'>
-            <div className='name'>name</div>
-            <div className='lastmsg'>msg</div>
-          </div>
-        </div>
-        <div className='item'>
-          <img src="./profile.png" alt="" />
-          <div className='texts'>
-            <div className='name'>name</div>
-            <div className='lastmsg'>msg</div>
-          </div>
-        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default ChatList;
