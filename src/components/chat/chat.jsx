db8d4497b8954187a79f7fd9cf1d1222f0bf94ac
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-04 11:54:54 +0000

    update

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -4,17 +4,17 @@ import EmojiPicker from 'emoji-picker-react'
 import { useGlobalState } from '../../backend/globalStates'
 
 const Chat = () => {
-
+ 
   const { currentChatUID, messages} = useGlobalState()
 
   const[open, setOpen] = useState(false)
-  const[text, setText] = useState("")
+  const[text, setText] = useState("")4
   const handleEmoji =(emoji) =>{
     setText((prev) => prev+emoji.emoji)
     setOpen(false)
   }
   return (
-    {
+    {currentChatUID ? (
     <div className='chat'>
       <div className="top">
         <div className="user">
@@ -25,7 +25,7 @@ const Chat = () => {
       <div className="center">    
         <div className="message">
           <div className='texts'>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti eveniet voluptate dolores cumque, libero quidem expedita, enim ipsa cup reprehenderit, maxime iure! Atque, vel esse al</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corupti eveniet voluptate dolores cumque, libero quidem expedita, enim ipsa cup reprehenderit, maxime iure! Atque, vel esse al</p>
             <div>1 pm</div>
           </div>
         </div>
@@ -43,7 +43,10 @@ const Chat = () => {
         </div>
         <button type="button" className='sendButton'>Send</button>
       </div>
-    </div>}
+    </div>)
+    :
+    <div>No chat selected</div>
+  }
   )
 }
 
