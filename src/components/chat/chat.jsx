fe4536782fca3f270806e6aeec9426be9b852e35
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-04 19:54:36 +0530

    Update chat.jsx

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -7,38 +7,7 @@ const Chat = () => {
  
   const { currentChatUID, messages} = useGlobalState()
   const [ msgs, setmsgs] = useState(messages)
-  const [userData, setUserData] = useState(null)
-  
-  const[open, setOpen] = useState(false)
-  const[text, setText] = useState("")
-  const handleEmoji =(emoji) =>{
-    setText((prev) => prev+emoji.emoji)
-    setOpen(false)
-  }
-  useEffect(async ()=>{
-    if(currentChatUID){
-      const ids = currentChatUID.split("_")
-      const otherUserUID = ids.filter(uid => uid !== currentUser.uid)[0]; 
-      const userDocRef = doc(db, "users", otherUserUID);
-      const userDocSnapshot = await getDoc(userDocRef);
-      if (userDocSnapshot.exists()) {
-        setUserData(userDocSnapshot.data());
-      }
-    }
-    const messagesRef = collection(db, "chats", currentChatUID, "messages");
-    const q = query(messagesRef); 
-
-    return onSnapshot(q, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-      if (change.type === "added") {
-        setmsgs(prevMsgs => [
-              ...prevMsgs,
-              { id: change.doc.id, ...change.doc.data() }
-            ]);
-      }
-    });
-  });
-  },[currentChatUID])
+ 
   return currentChatUID ? (
     <div className='chat'>
       <div className="top">
