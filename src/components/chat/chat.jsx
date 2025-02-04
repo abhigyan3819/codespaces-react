3b238763193b693d325df6f8c87d777e94b016ab
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-04 19:16:05 +0530

    Update chat.jsx

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -6,7 +6,7 @@ import { useGlobalState } from '../../backend/globalStates'
 const Chat = () => {
  
   const { currentChatUID, messages} = useGlobalState()
-  const [ msgs, setImgs] = useState([messages])
+  const [ msgs, setmsgs] = useState(messages)
   
   const[open, setOpen] = useState(false)
   const[text, setText] = useState("")
@@ -30,53 +30,51 @@ const Chat = () => {
     return onSnapshot(q, (snapshot) => {
       snapshot.docChanges().forEach((change) => {
       if (change.type === "added") {
-        setImgs(...msgs,{
-          id: change.doc.id,
-          ...change.doc.data(),
-        });
+        setmsgs(prevMsgs => [
+              ...prevMsgs,
+              { id: change.doc.id, ...change.doc.data() }
+            ]);
       }
     });
   });
-  },[])
-  return (
-  currentChatUID ? (
+  },[currentChatUID])
+  return currentChatUID ? (
     <div className='chat'>
       <div className="top">
         <div className="user">
           <img src="./profile.png" alt="" />
-          <div className='name'>userData?.username</div>
+          <div className='name'>{userData?.username || "Unknown"}</div>
         </div>
       </div>
       <div className="center">
-        {msgs?.map((msg)=>{
-        <div className="message">
-          <div className='texts'>
-            <p>{msg.text}</p>
-            <div>{msg.timestamp}</div>
+        {msgs?.map((msg) => (
+          <div className="message" key={msg.id}>
+            <div className='texts'>
+              <p>{msg.text}</p>
+              <div>{msg.timestamp?.toDate().toLocaleString()}</div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-      })}
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
         </div>
-        <input type="text" placeholder='Type a message...' onChange={(e)=>setText(e.target.value)} value={text}/>
+        <input type="text" placeholder='Type a message...' onChange={(e) => setText(e.target.value)} value={text} />
         <div className='emoji'>
-          <img src="./emoji.png" alt="" onClick={()=>setOpen((prev)=>!prev)} />
-          {open && (  
+          <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)} />
+          {open && (
             <div className="picker">
-              <EmojiPicker onEmojiClick={handleEmoji}/>
+              <EmojiPicker onEmojiClick={handleEmoji} />
             </div>
           )}
         </div>
-        <button type="button" className='sendButton'>Send</button>
+        <button type="button" className='sendButton' onClick={sendMessage}>Send</button>
       </div>
     </div>
   ) : (
     <div className='chat no-chat'>No chat selected</div>
-  )
-);
+  );
 }
 
 export default Chat;
