03569a69edffaaf5f4b26dc51aacbc05762ed858
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-05 19:25:25 +0530

    Update chat.jsx

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -6,22 +6,53 @@ import { useGlobalState } from '../../backend/globalStates'
 const Chat = () => {
  
   const { currentChatUID, messages} = useGlobalState()
-  const [ msgs, setmsgs] = useState([])
- 
+  const [ msgs, setmsgs] = useState(messages)
+  const [userData, setUserData] = useState(null)
+  
+  const[open, setOpen] = useState(false)
+  const[text, setText] = useState("")
+  const handleEmoji =(emoji) =>{
+    setText((prev) => prev+emoji.emoji)
+    setOpen(false)
+  }
+  useEffect(async ()=>{
+    if(currentChatUID){
+      const ids = currentChatUID.split("_")
+      const otherUserUID = ids.filter(uid => uid !== currentUser.uid)[0]; 
+      const userDocRef = doc(db, "users", otherUserUID);
+      const userDocSnapshot = await getDoc(userDocRef);
+      if (userDocSnapshot.exists()) {
+        setUserData(userDocSnapshot.data());
+      }
+    }
+    const messagesRef = collection(db, "chats", currentChatUID, "messages");
+    const q = query(messagesRef); 
+
+    return onSnapshot(q, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+      if (change.type === "added") {
+        setmsgs(prevMsgs => [
+              ...prevMsgs,
+              { id: change.doc.id, ...change.doc.data() }
+            ]);
+      }
+    });
+  });
+  },[currentChatUID])
   return currentChatUID ? (
     <div className='chat'>
       <div className="top">
         <div className="user">
           <img src="./profile.png" alt="" />
-          <div className='name'>userData?.username || "Unknown"</div>
+          <div className='name'>{userData?.username || "Unknown"}</div>
         </div>
       </div>
       <div className="center">
         {msgs?.map((msg) => (
           <div className="message" key={msg.id}>
             <div className='texts'>
-              <p>msg.text</p>
-              <div>msg.timestamp?.toDate().toLocaleString()</div>
+              <p>{msg.text}</p>
+              <div>{msg.timestamp?.toDate().toLocaleString()}</div>
             </div>
           </div>
         ))}
@@ -39,7 +70,7 @@ const Chat = () => {
             </div>
           )}
         </div>
-        <button type="button" className='sendButton' >Send</button>
+        <button type="button" className='sendButton' onClick={sendMessage}>Send</button>
       </div>
     </div>
   ) : (
